b740fae908b21d02113037ed34ed2f268737c67b
dewanhasan99 <g00410156@atu.ie>
2023-10-09 21:59:16 +0100

    First Commit to MAin

---
+++ src/main/java/ie/atu/lab3ex3cicd/Lab3Ex3CicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3ex3cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Ex3CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3Ex3CicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab3ex3cicd/Lab3Ex3CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab3ex3cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3Ex3CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
