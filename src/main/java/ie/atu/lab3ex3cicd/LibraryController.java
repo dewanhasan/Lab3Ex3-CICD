fff215c9a1a0900ad4aa2ee6706b2be34efe91aa
dewanhasan99 <g00410156@atu.ie>
2023-10-10 22:54:12 +0100

    Second Commit

--- src/main/java/ie/atu/lab3ex3cicd/LibraryController.java
+++ src/main/java/ie/atu/lab3ex3cicd/LibraryController.java
@@ -1,5 +1,27 @@
 package ie.atu.lab3ex3cicd;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class LibraryController {
+
+    @Autowired
+    private  LibraryService libraryService;
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public LibraryService addBookstoLib(@RequestBody LibraryService libraryService){
+        return libraryService;
+    }
+
+    @GetMapping("/get")
+    public List<Book> getBooks(){
+        return libraryService.getBooks();
+    }
+
 }
